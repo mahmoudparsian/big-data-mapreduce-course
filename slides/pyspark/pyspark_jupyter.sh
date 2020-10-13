58227e71646bec0f8f70d7d6fc9229622709cca5
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2020-10-12 19:55:06 -0700

    added pyspark_jupyter.sh

---
+++ slides/pyspark/pyspark_jupyter.sh
@@ -0,0 +1,5 @@
+export SPARK_HOME=/Users/mparsian/spark-3.0.0
+export PATH=$SPARK_HOME/bin:$PATH
+export PYSPARK_DRIVER_PYTHON=jupyter
+export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
+/Users/mparsian/spark-3.0.0/bin/pyspark

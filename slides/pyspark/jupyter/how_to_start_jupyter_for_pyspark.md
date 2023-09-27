cf210a6716af7d5b1d8616cce552c9e7e745fc6f
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2023-09-27 15:52:34 -0700

    updated pyspark jupyter access

--- slides/pyspark/jupyter/how_to_start_jupyter_for_pyspark.md
+++ slides/pyspark/jupyter/how_to_start_jupyter_for_pyspark.md
@@ -1,7 +1,11 @@
-````
-export SPARK_HOME=/home/mparsian/spark-3.1.2
-export PATH=$SPARK_HOME/bin:$PATH
-export PYSPARK_DRIVER_PYTHON=jupyter
-export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
-$SPARK_HOME/bin/pyspark
-````
+# 1. Create a file pyspark_jupyter.sh
+
+Update directories accordingly
+
+# 2. Execute `pyspark_jupyter.sh` as:
+
+		source pyspark_jupyter.sh
+		
+# 3. Then try some Spark commands:
+
+![](./pyspark_jupyter.png)
--- slides/pyspark/jupyter/pyspark_jupyter.sh
+++ slides/pyspark/jupyter/pyspark_jupyter.sh
@@ -1,5 +1,17 @@
-export SPARK_HOME=/Users/mparsian/spark-3.0.0
-export PATH=$SPARK_HOME/bin:$PATH
+# 1. define where is Python
+export PYTHON_HOME=/Users/mparsian/Library/Python/3.10
+
+# 2. define where is Spark
+export SPARK_HOME=/Users/mparsian/spark-3.5.0
+
+# 3. set PATH
+export PATH=$PYTHON_HOME/bin:$SPARK_HOME/bin:$PATH
+
+# 4. define PySpark driver
 export PYSPARK_DRIVER_PYTHON=jupyter
+
+# 5. define your notebook
 export PYSPARK_DRIVER_PYTHON_OPTS='notebook'
-/Users/mparsian/spark-3.0.0/bin/pyspark
+
+# 6. Invoke PySpark and use it from Jupyter
+$SPARK_HOME/bin/pyspark

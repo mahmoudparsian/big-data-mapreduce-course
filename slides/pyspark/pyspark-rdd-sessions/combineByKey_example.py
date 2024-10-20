a9d4f328beb743e052954cc5b75db223ca3647cb
Mahmoud Parsian <mahmoud.parsian@yahoo.com>
2024-10-20 14:09:52 -0700

    updated docs

--- slides/pyspark/pyspark-rdd-sessions/combineByKey_example.py
+++ slides/pyspark/pyspark-rdd-sessions/combineByKey_example.py
@@ -1,11 +1,10 @@
 Problem: Given a set of (K, V) pairs,
-find (sum, count, min, max) per key using 
-the combineByKey() transformation.
+find (average, min, max) per key 
+using the combineByKey() transformation.
 
-~/spark-2.4.4 $ ./bin/pyspark
+$ ./bin/pyspark
 Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:44:43)
 [Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license" for more information.
 Welcome to
       ____              __
      / __/__  ___ _____/ /__
@@ -20,26 +19,48 @@ SparkSession available as 'spark'.
 >>>
 >>> spark
 <pyspark.sql.session.SparkSession object at 0x119f71668>
->>> data = [('A', 2), ('A', 3), ('A', 4), ('A', 5), ('B', 6), ('B', 7), ('B', 8) ]
+>>>
+>>> data = [('A', 2), ('A', 3), ('A', 4), ('A', 5), 
+            ('B', 6), ('B', 7), ('B', 8) ]
 >>> data
-[('A', 2), ('A', 3), ('A', 4), ('A', 5), ('B', 6), ('B', 7), ('B', 8)]
+[('A', 2), ('A', 3), ('A', 4), ('A', 5), 
+ ('B', 6), ('B', 7), ('B', 8)]
+
+>>> # Create an RDD[(K, V)]
+>>> # rdd: RDD[(String, Integer)]
 >>> rdd = spark.sparkContext.parallelize(data)
 >>>
 >>>
 >>> rdd.count()
 7
 >>> rdd.collect()
-[('A', 2), ('A', 3), ('A', 4), ('A', 5), ('B', 6), ('B', 7), ('B', 8)]
+[('A', 2), ('A', 3), ('A', 4), ('A', 5), 
+ ('B', 6), ('B', 7), ('B', 8)]
+ 
+>>> # Using combineByKey(), create:
 >>> # (K, (sum, count, min, max))
-...
+
+>>> # Define 3 functions for combineByKey():
+>>> # Combined data type is (sum, count, min, max)
+
+>>> # create a C as (sum, count, min, max)
 >>> def single(v):
-...    return (v, 1, v, v)
+...    #      (sum, count, min, max)
+...    return (v,   1,     v,   v)
 ...
+>>> # C = (C[0]=sum, C[1]=count, C[2]=min, C[3]=max)
 >>> def merge(C, v):
 ...    return (C[0]+v, C[1]+1, min(C[2],v), max(C[3],v))
 ...
->>> def combine(C1, C2):
-...    return (C1[0]+C2[0], C1[1]+C2[1], min(C1[2], C2[2]), max(C1[3], C2[3]) )
+>>> # C = (C[0]=sum, C[1]=count, C[2]=min, C[3]=max)
+>>> # D = (D[0]=sum, D[1]=count, D[2]=min, D[3]=max)
+>>> def combine(C, D):
+...    return (
+               C[0]+D[0], 
+               C[1]+D[1], 
+               min(C[2], D[2]), 
+               max(C[3], D[3]) 
+              )
 ...
 >>> rdd2 = rdd.combineByKey(single, merge, combine)
 >>> rdd2.collect()
@@ -47,4 +68,10 @@ SparkSession available as 'spark'.
  ('B', (21, 3, 6, 8)), 
  ('A', (14, 4, 2, 5))
 ]
-
+>>>
+>>> final = rdd2.mapValues(lambda t: (float(t[0])/t[1], t[2], t[3]))
+>>> final.collect()
+[
+ ('B', (7.0, 6, 8)), 
+ ('A', (3.5, 2, 5))
+]
--- slides/pyspark/pyspark-rdd-sessions/count_min_max.py
+++ slides/pyspark/pyspark-rdd-sessions/count_min_max.py
@@ -26,39 +26,35 @@ spark = SparkSession\
 # APPLY a SET of TRANSFORMATIONS...
 
 #-------------------------------------------
-def minmax(partition):
-	first_time = False
-	#count
-	#min2
-	#max2
+def count_min_max(partition):
+	first_time = True
 	for x in partition:
-		if (first_time == False):
-			count = 1
-			min2 = x
-			max2 = x
-			first_time = True
+		if (first_time):
+			local_count = 1
+			local_min = x
+			local_max = x
+			first_time = False
 		else:
-			count = count + 1
-			max2 = max(x, max2)
-			min2 = min(x, min2)
+			local_count += 1
+			local_max = max(x, local_max)
+			local_min = min(x, local_min)
 	#end-for
 	#
-	return [(count, min2, max2)]
+	return [(local_count, local_min, local_max)]
 #end-def
 #---------------------
 def iterate_partition(partition):
-   elements = []
-   for x in partition:
-      elements.append(x)
-   print("elements=", elements)
+   print("elements=", list(partition))
    #print ("==================")
 #end-def
 #-------------------------
+# t1 = (count1, min1, max1)
+# t2 = (count2, min2, max2)
 def add3(t1, t2):
 	count = t1[0] + t2[0]
-	min2 = min(t1[1], t2[1])
-	max2 = max(t1[2], t2[2])
-	return (count, min2, max2)
+	minimum = min(t1[1], t2[1])
+	maximum = max(t1[2], t2[2])
+	return (count, minimum, maximum)
 #end-def
 
 data = [10, 20, 30, 44, 55, 3, 4, 60, 50, 5, 2, 2, 20, 20, 10, 30, 70]
@@ -74,8 +70,8 @@ rdd.foreachPartition(iterate_partition)
 print("==============")
 #
 
-count_min_max_rdd = rdd.mapPartitions(minmax)
-print("minmax_rdd.collect()=", count_min_max_rdd.collect())
+count_min_max_rdd = rdd.mapPartitions(count_min_max)
+print("count_min_max_rdd.collect()=", count_min_max_rdd.collect())
 
 final_triplet = count_min_max_rdd.reduce(add3)
 print("final_triplet=", final_triplet)
